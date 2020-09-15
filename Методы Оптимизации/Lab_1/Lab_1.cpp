6778ec84a41da80984cc533ff638bc366cc10767
AlexHoz <alexhozov00@gmail.com>
2020-09-15 16:19:57 +0500

    Переписал 1-ю лабу через указатели

--- Методы Оптимизации/Lab_1/Lab_1.cpp
+++ Методы Оптимизации/Lab_1/Lab_1.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 double Function(double x) 
 {
-    return max(abs(x)+1,x*x+4*x+4);
+    return max(abs(x) + 1, x * x + 4 * x + 4);
    //Вы должны вернуть то что посчитала ВАША функция
 }
 
-double DivisionMethod(double accuracy, double a, double b)
+double DivisionMethod(double accuracy, double a, double b, double (*function)(double))
 {
     double c = (a + b) / 2;
     double x, y, Fx, Fy, Fc = Function(c);
@@ -17,8 +17,8 @@ double DivisionMethod(double accuracy, double a, double b)
     {
         x = a + (b - a) / 4;
         y = b - (b - a) / 4;
-        Fx = Function(x);
-        Fy = Function(y);
+        Fx = function(x);
+        Fy = function(y);
         if (Fx < Fc) {
             b = c;
             c = x;
@@ -37,7 +37,7 @@ double DivisionMethod(double accuracy, double a, double b)
     return c;
 }
 
-double DichotomyMethod(double accuracy, double distinguishability, double a, double b)
+double DichotomyMethod(double accuracy, double distinguishability, double a, double b, double (*function)(double))
 {
     double c = (a + b) / 2;
     double x, y, Fx, Fy;
@@ -45,22 +45,17 @@ double DichotomyMethod(double accuracy, double distinguishability, double a, dou
     while (b - a > accuracy) {
         x = (a + b) / 2 - distinguishability;
         y = (a + b) / 2 + distinguishability;
-        Fx = Function(x);
-        Fy = Function(y);
-        if (Fx > Fy) {
-            a = x;
-        }
-        else if (Fx <= Fy) {
-            b = y;
-        }
+        Fx = function(x);
+        Fy = function(y);
+        Fx > Fy ? a = x : b = y;;
     }
     return (a + b) / 2;
 }
 
 int main()
 {
-    cout << DivisionMethod(0.01, -5, 3) << endl
-        << DichotomyMethod(0.01, 0.001, -5, 3);
+    cout << DivisionMethod(0.01, -5, 3, Function) << endl
+        << DichotomyMethod(0.01, 0.001, -5, 3, Function);
     //a = -5 и  b = 3 - это мои параметры, измените на свои
     //0.01 - заданная точность
     //0.001 - погрешность, как я понял её придумываем сами, но по условию (погрешность * 2 < точности)
