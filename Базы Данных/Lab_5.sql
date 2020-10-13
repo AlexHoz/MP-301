193722953599359b846bc70d8265e27b5c6ac51d
Andrew Mikhalev <43449316+ITDarkUFO@users.noreply.github.com>
2020-10-13 20:50:04 +0500

    Упорядочил задания, заменил задание №63

--- Базы Данных/Lab_5.sql
+++ Базы Данных/Lab_5.sql
@@ -1,3 +1,78 @@
+--Задание 37
+SELECT class
+  FROM (SELECT class, name
+          FROM Ships
+        UNION
+        SELECT Outcomes.ship, Outcomes.ship
+          FROM Outcomes, classes
+         WHERE outcomes.ship = classes.class) as t1
+ GROUP BY class
+HAVING COUNT(name) = 1
+
+--Задание 48
+SELECT class
+  FROM Ships
+ WHERE name in (SELECT DISTINCT ship 
+                  FROM outcomes 
+                 WHERE result = 'sunk')
+UNION 
+SELECT ship
+  FROM Outcomes
+ WHERE result = 'sunk' AND ship IN (SELECT class FROM classes)
+ 
+ --Задание 49
+SELECT name
+  FROM Ships
+ WHERE class IN (SELECT class
+                   FROM Classes 
+                  WHERE bore = 16)
+UNION
+SELECT ship as name
+  FROM Outcomes
+ WHERE ship IN (SELECT class
+                   FROM Classes 
+                  WHERE bore = 16)
+
+--Задание 50
+SELECT DISTINCT battle
+  FROM Outcomes
+ WHERE Outcomes.ship IN (SELECT name
+                           FROM Ships
+                          WHERE class = 'Kongo')
+
+--Задание 63
+SELECT name
+FROM passenger
+WHERE id_psg
+IN
+(
+	SELECT ID_psg
+	FROM pass_in_trip
+	GROUP BY ID_psg, place
+	HAVING count(place) > 1
+)
+
+--Задание 74
+SELECT Country, Class
+  FROM Classes
+ WHERE Country = 'Russia'
+UNION
+SELECT Country, Class
+  FROM Classes
+ WHERE NOT EXISTS (SELECT Country, Class
+                     FROM Classes
+                    WHERE Country = 'Russia')
+
+--Задание 85
+SELECT maker
+  FROM (SELECT maker, MAX(type) AS type, COUNT(DISTINCT model) AS count_model
+          FROM Product
+         GROUP BY maker
+        HAVING COUNT(DISTINCT type) = 1
+       ) AS X
+ WHERE (type = 'Printer') OR (count_model > 2 AND type = 'PC')
+
+--Дополнительные задания из лабораторной
 -- 1.2
 SELECT *
   FROM utQ
@@ -7,16 +82,19 @@ SELECT *
 
 SELECT *
   FROM utB
+  
 -- 1.3
 SELECT * 
   FROM utQ 
  WHERE Q_ID IN (SELECT DISTINCT B_Q_ID
                   FROM utB)
+                  
 -- 1.4
 SELECT * 
   FROM utQ 
  WHERE Q_ID NOT IN (SELECT DISTINCT B_Q_ID
                   FROM utB)
+                  
 -- 1.5
 SELECT Q_NAME
   FROM utQ
@@ -25,6 +103,7 @@ WHERE Q_ID IN (SELECT B_Q_ID
                          FROM utB
                         GROUP BY B_Q_ID) as sum_paint
                  WHERE total_paint = 765)
+                 
 -- 1.6
 SELECT Q_NAME
   FROM utQ
@@ -33,6 +112,7 @@ WHERE Q_ID IN (SELECT B_Q_ID
                          FROM utB
                         GROUP BY B_Q_ID) as paints
                 WHERE count_paints > 3)
+                
 -- 1.7
 SELECT Q_NAME
   FROM utQ
@@ -41,6 +121,7 @@ WHERE Q_ID IN (SELECT B_Q_ID
                          FROM utB
                         GROUP BY B_Q_ID) as paints
                 WHERE count_paints > 3)
+                
 -- 1.9
 SELECT Q_NAME
   FROM utQ
@@ -54,6 +135,7 @@ SELECT Q_NAME
                           FROM utB
                          GROUP BY B_Q_ID) as paints
                  WHERE different_paints = 2)
+                 
 -- 1.10
 SELECT Q_NAME
   FROM utQ
@@ -63,6 +145,7 @@ SELECT Q_NAME
                                     FROM utV
                                    WHERE V_COLOR = 'G')
                )
+               
 -- 1.11
 SELECT Q_NAME
   FROM utQ
@@ -77,76 +160,14 @@ SELECT Q_NAME
                                         FROM utV
                                        WHERE V_COLOR = 'R')
                    )
--- 2.1(85)
-SELECT maker
-  FROM (SELECT maker, MAX(type) AS type, COUNT(DISTINCT model) AS count_model
-          FROM Product
-         GROUP BY maker
-        HAVING COUNT(DISTINCT type) = 1
-       ) AS X
- WHERE (type = 'Printer') OR (count_model > 2 AND type = 'PC')
--- 2.2(74)
-SELECT Country, Class
-  FROM Classes
- WHERE Country = 'Russia'
-UNION
-SELECT Country, Class
-  FROM Classes
- WHERE NOT EXISTS (SELECT Country, Class
-                     FROM Classes
-                    WHERE Country = 'Russia')
--- 2.3(50)
-SELECT DISTINCT battle
-  FROM Outcomes
- WHERE Outcomes.ship IN (SELECT name
-                           FROM Ships
-                          WHERE class = 'Kongo')
--- 2.3(49)
-SELECT name
-  FROM Ships
- WHERE class IN (SELECT class
-                   FROM Classes 
-                  WHERE bore = 16)
-UNION
-SELECT ship as name
-  FROM Outcomes
- WHERE ship IN (SELECT class
-                   FROM Classes 
-                  WHERE bore = 16)
--- 2.3(48)
-SELECT class
-  FROM Ships
- WHERE name in (SELECT DISTINCT ship 
-                  FROM outcomes 
-                 WHERE result = 'sunk')
-UNION 
-SELECT ship
-  FROM Outcomes
- WHERE result = 'sunk' AND ship IN (SELECT class FROM classes)
--- 2.4(37)
-SELECT class
-  FROM (SELECT class, name
-          FROM Ships
-        UNION
-        SELECT Outcomes.ship, Outcomes.ship
-          FROM Outcomes, classes
-         WHERE outcomes.ship = classes.class) as t1
- GROUP BY class
-HAVING COUNT(name) = 1
+
 -- 2.5
 SELECT *
   FROM Passenger
 
 SELECT *
   FROM Pass_in_trip
--- 2.6(63)
-SELECT name
-  FROM Passenger
- WHERE ID_psg IN (SELECT ID_psg
-                    FROM Pass_in_trip
-                   GROUP BY ID_psg
-                  HAVING COUNT(DISTINCT place) < COUNT(place)
-                 )
+
 -- 2.7
 SELECT DISTINCT name
   FROM Passenger
@@ -156,6 +177,7 @@ SELECT DISTINCT name
                                        FROM Trip
                                       WHERE town_to = 'Rostov')
                  )
+                 
 -- 2.8
 SELECT DISTINCT name
   FROM Passenger
