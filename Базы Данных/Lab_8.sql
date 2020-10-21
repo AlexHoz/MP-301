e47f485471d6d87ca1ceb83e8700b2096633e956
Andrew Mikhalev <43449316+ITDarkUFO@users.noreply.github.com>
2020-10-21 13:04:45 +0500

    Update Lab_8.sql

--- Базы Данных/Lab_8.sql
+++ Базы Данных/Lab_8.sql
@@ -1,12 +1,18 @@
 --Задание 77
 WITH t AS
 (
-SELECT date, count(DISTINCT trip_no) AS count_trip
-FROM Pass_in_trip
-WHERE trip_no IN (SELECT trip_no FROM trip WHERE town_from = 'Rostov')
-GROUP BY date
+  SELECT date, count(DISTINCT trip_no) AS count_trip
+  FROM Pass_in_trip
+  WHERE trip_no IN 
+  (
+    SELECT trip_no FROM trip WHERE town_from = 'Rostov'
+  )
+  GROUP BY date
 )
 
 SELECT count_trip, date
 FROM t
-WHERE count_trip = (SELECT max(count_trip) FROM t)
+WHERE count_trip = 
+(
+  SELECT max(count_trip) FROM t
+)
